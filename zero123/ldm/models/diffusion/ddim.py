f62a9aa536057c59bbdca7742647d278cdf9e13a
Jiho Choi <choi.scholarg@gmail.com>
2025-06-07 18:09:02 +0000

    minor fix

--- zero123/ldm/models/diffusion/ddim.py
+++ zero123/ldm/models/diffusion/ddim.py
@@ -211,7 +211,7 @@ class DDIMSampler(object):
             #     c_in = torch.cat([unconditional_conditioning, c])
             # e_t_uncond, e_t = self.model.apply_model(x_in, t_in, c_in).chunk(2)
             e_t = self.model.apply_model(x, t, c)
-            import ipdb; ipdb.set_trace()
+            # import ipdb; ipdb.set_trace()
             e_t_uncond = self.model.apply_model(x, t, unconditional_conditioning)
             
             e_t = e_t_uncond + unconditional_guidance_scale * (e_t - e_t_uncond)
